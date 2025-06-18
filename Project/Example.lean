2ac9f8b75333292987eee6d753a854a98fcba8fe
xidbad <etocando4678@gmail.com>
2025-06-18 23:51:04 +0900

    only mathlib

--- Project/Example.lean
+++ Project/Example.lean
@@ -1,16 +1,17 @@
-import Mathlib.NumberTheory.ArithmeticFunction
-import Mathlib.NumberTheory.LucasLehmer
-import Mathlib.Algebra.GeomSum
-import Mathlib.RingTheory.Multiplicity
-import Mathlib.Tactic.NormNum.Prime
+import Mathlib
+-- import Mathlib.NumberTheory.ArithmeticFunction
+-- import Mathlib.NumberTheory.LucasLehmer
+-- import Mathlib.Algebra.GeomSum
+-- import Mathlib.RingTheory.Multiplicity
+-- import Mathlib.Tactic.NormNum.Prime
 
-import Mathlib.Algebra.BigOperators.Ring.Finset
-import Mathlib.Algebra.Module.BigOperators
-import Mathlib.NumberTheory.Divisors
-import Mathlib.Data.Nat.Squarefree
-import Mathlib.Data.Nat.GCD.BigOperators
-import Mathlib.Data.Nat.Factorization.Induction
-import Mathlib.Tactic.ArithMult
+-- import Mathlib.Algebra.BigOperators.Ring.Finset
+-- import Mathlib.Algebra.Module.BigOperators
+-- import Mathlib.NumberTheory.Divisors
+-- import Mathlib.Data.Nat.Squarefree
+-- import Mathlib.Data.Nat.GCD.BigOperators
+-- import Mathlib.Data.Nat.Factorization.Induction
+-- import Mathlib.Tactic.ArithMult
 
 open Nat
 
