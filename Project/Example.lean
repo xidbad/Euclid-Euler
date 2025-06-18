17144d9734736123b769fe7ddfd5990589a0798f
xidbad <etocando4678@gmail.com>
2025-06-18 14:24:52 +0900

    def sigma

---
+++ Project/Example.lean
@@ -0,0 +1,29 @@
+import Mathlib.NumberTheory.ArithmeticFunction
+import Mathlib.NumberTheory.LucasLehmer
+import Mathlib.Algebra.GeomSum
+import Mathlib.RingTheory.Multiplicity
+import Mathlib.Tactic.NormNum.Prime
+
+import Mathlib.Algebra.BigOperators.Ring.Finset
+import Mathlib.Algebra.Module.BigOperators
+import Mathlib.NumberTheory.Divisors
+import Mathlib.Data.Nat.Squarefree
+import Mathlib.Data.Nat.GCD.BigOperators
+import Mathlib.Data.Nat.Factorization.Induction
+import Mathlib.Tactic.ArithMult
+
+open Finset
+
+open Nat
+
+open ArithmeticFunction Finset
+
+namespace Theorems100
+
+namespace Nat
+
+namespace ArithmeticFunction
+
+/-- `σ k n` is the sum of the `k`th powers of the divisors of `n` -/
+def sigma (k : ℕ) : ArithmeticFunction ℕ :=
+  ⟨fun n => ∑ d ∈ divisors n, d ^ k, by simp⟩
